4fb32c0394067d11dcc98f5e89584e08cbb204f0
Kien-LeChi <chikienl@metropolia.fi>
2025-12-04 19:53:41 +0200

    Update app.py
    Update app.py to host endpoints to all of the pages' views

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from api import api_bp
+
 app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app)
@@ -14,6 +15,29 @@ def index():
         return render_template('index.html')
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
+
+@app.route('/syllabus-panel/')
+def syllabus_panel():
+    try:
+        return render_template('syllabus-panel.html')
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+    
+@app.route('/create-syllabus')
+def create_syllabus():
+    try:
+        return render_template('create-syllabus.html')
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+    
+@app.route('/view-syllabus')
+def view_syllabus():
+    try:
+        return render_template('view-syllabus.html')
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+
 if __name__ == '__main__':
     print("App is running")
     app.run(debug=True, host='0.0.0.0', port=5000)
